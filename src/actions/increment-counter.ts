a896db6f76dd264d37d649f74c1578f74bd98099
Bruno St John <brunost.john@icloud.com>
2024-12-05 22:20:28 +0000

    initial commit

---
+++ src/actions/increment-counter.ts
@@ -0,0 +1,29 @@
+import {
+  action,
+  KeyDownEvent,
+  SingletonAction,
+  WillAppearEvent,
+} from "@elgato/streamdeck";
+
+@action({ UUID: "com.brunostjohn.sdkubernetes.increment" })
+export class IncrementCounter extends SingletonAction<CounterSettings> {
+  override onWillAppear(
+    ev: WillAppearEvent<CounterSettings>
+  ): void | Promise<void> {
+    return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
+  }
+
+  override async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
+    const { settings } = ev.payload;
+    settings.incrementBy ??= 1;
+    settings.count = (settings.count ?? 0) + settings.incrementBy;
+
+    await ev.action.setSettings(settings);
+    await ev.action.setTitle(`${settings.count}`);
+  }
+}
+
+type CounterSettings = {
+  count?: number;
+  incrementBy?: number;
+};
